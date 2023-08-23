b50bc6e4b19a896abc791f09a9cb98e68cba811b
HernanGoldy <hernanmdq@gmail.com>
2023-08-22 22:09:47 -0300

    Agrego getter y setter de valor

--- sesion04/src/main/java/com/openbootcamp/models/Bootcamper.java
+++ sesion04/src/main/java/com/openbootcamp/models/Bootcamper.java
@@ -18,4 +18,7 @@ public class Bootcamper {
     // getters and setters
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public double getValor() { return valor; }
+    public void setValor(double valor) { this.valor = valor; }
 }
