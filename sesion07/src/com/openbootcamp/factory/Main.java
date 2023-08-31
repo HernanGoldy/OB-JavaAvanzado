074adf8b534efef26bc1caa1ffc89f313f01a6ca
HernanGoldy <hernanmdq@gmail.com>
2023-08-31 08:59:41 -0300

    Instancio la interface Precio en PrecioFactory

--- sesion07/src/com/openbootcamp/factory/Main.java
+++ sesion07/src/com/openbootcamp/factory/Main.java
@@ -1,4 +1,11 @@
 package com.openbootcamp.factory;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        PrecioFactory precio = new PrecioFactory("España");
+
+        PrecioFactory precio2 = new PrecioFactory("USA");
+    }
 }
--- sesion07/src/com/openbootcamp/factory/PrecioFactory.java
+++ sesion07/src/com/openbootcamp/factory/PrecioFactory.java
@@ -5,4 +5,25 @@ Aquí implemento el patrón Factory.
  */
 
 public class PrecioFactory {
+
+    // atributos (características)
+    Precio precio; // es una instancia
+
+    // constructores
+    private PrecioFactory() {};
+
+    public PrecioFactory(String pais) {
+        /*
+        pais = España -> PrecioEUR
+        pais != España -> PrecioUSD
+        */
+
+        if (pais.equalsIgnoreCase("España")) {
+            System.out.println("España, precio en EUR");
+            precio = new PrecioEUR();
+        } else {
+            System.out.println("Otro país, precio en USD");
+            precio = new PrecioUSD();
+        }
+    }
 }
